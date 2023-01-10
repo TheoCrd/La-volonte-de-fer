e509ae3e4fb728f2af8acc11abf874c367021175
TheoCrd <theo.couard@hotmail.fr>
2023-01-10 17:33:39 +0100

    added interaction

---
+++ Assets/ActivateTeleportationRay.cs
@@ -0,0 +1,27 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.XR.Interaction.Toolkit;
+using UnityEngine.InputSystem;
+
+public class ActivateTeleportationRay : MonoBehaviour
+{
+    public GameObject leftTeleportation;
+    public GameObject rightTeleportation;
+
+    public InputActionProperty leftActivate;
+    public InputActionProperty rightActivate;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        leftTeleportation.SetActive(leftActivate.action.ReadValue<float>() > 0.1f);
+        rightTeleportation.SetActive(rightActivate.action.ReadValue<float>() > 0.1f);
+    }
+}
