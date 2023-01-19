07869c8a1bb82804dd5544d282de662f7ac3fae4
TheoCrd <theo.couard@hotmail.fr>
2023-01-19 11:10:52 +0100

    script for scene transition done

--- Assets/SceneSwitch.cs
+++ Assets/SceneSwitch.cs
@@ -7,31 +7,16 @@ public class SceneSwitch : MonoBehaviour
 {
 
     public Animator animator;
-
-    void OnTriggerEnter(Collider col)
-    {
-        if (col.name == "Cube2")
-        { 
-            StartCoroutine(wait());
-        }
-    }
+    private int levelToLoad;
 
     public void FadeToLevel(int sceneIndex)
     {
+        levelToLoad = sceneIndex;
         animator.SetTrigger("FadeOut");
     }
 
     public void OnFadeComplete()
     {
-        SceneManager.LoadScene(1);
-    }
-
-    IEnumerator wait()
-    {
-        //yield on a new YieldInstruction that waits for 5 seconds.
-        FadeToLevel(1);
-        yield return new WaitForSeconds(5);
-        OnFadeComplete();
-        //Debug.Log("Wait is over");
+        SceneManager.LoadScene(levelToLoad);
     }
 }
